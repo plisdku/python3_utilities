df29a8bf0e70a71d553eea9a4af9ee5a6454ddf6
Paul Hansen <paul.c.hansen@gmail.com>
2019-08-09 16:47:02 -0700

    bbedit_module can open the file containing a given function now

--- plisdku/shellthing.py
+++ plisdku/shellthing.py
@@ -1,10 +1,17 @@
+"""
+Something by
+Paul Hansen
+paul.c.hansen@gmail.com
+"""
+
 import os, sys
 import subprocess
 
 def _shell_that_path(cmd, path, directory=False):
-
     if hasattr(path, "__file__"):
         path = path.__file__
+    elif hasattr(path, "__code__"):
+        path = path.__code__.co_filename
 
     if directory:
         path = os.path.dirname(path)
